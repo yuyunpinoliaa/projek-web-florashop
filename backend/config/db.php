625f8e9fad8719fea86979075fa5ba705f5ed6fd
vinadwi <vina123@gmail.com>
2026-06-24 13:59:38 +0700

    menambahkan checkout.php ke halaman checkout #5

--- backend/config/db.php
+++ backend/config/db.php
@@ -6,7 +6,11 @@ $dbname = 'florashop'; // Ensure this database exists
 $username = 'root'; // Change if different
 $password = ''; // Change if different
 
-try 
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
 ?>
